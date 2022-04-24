792990fe9e29eb8a17238615b1e086d08c2fcda5
lvhaizhou <417703682@qq.com>
2022-04-24 17:37:40 +0800

    feat(app): init

---
+++ src/pages/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { defineComponent } from "vue";
+import Foo from "~/components/Foo";
+</script>
+
+<template>
+  <div>
+    <p>
+      <a href="https://github.com/hayeslv/vites/" target="_blank">Vites</a>
+    </p>
+    <p>
+      <em>Vite Starter Template</em>
+    </p>
+    <Foo />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+</style>
