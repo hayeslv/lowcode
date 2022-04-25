1feeebee90aa25bafe0b2d527b2454391b461737
lvhaizhou <417703682@qq.com>
2022-04-25 16:57:47 +0800

    feat(shared): 搭建shared项目

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+
+// "eslintConfig": {
+//   "extends": "@hayeslv",
+//   "ignorePatterns": ["auto-imports.d.ts"],
+//   "rules": {}
+// }
+const { defineConfig } = require("eslint-define-config");
+
+module.exports = defineConfig({
+  extends: "@hayeslv",
+});
