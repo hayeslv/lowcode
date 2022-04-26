68a366723fd679e88a575bbe1b80b4df3716f641
lvhaizhou <417703682@qq.com>
2022-04-26 17:02:32 +0800

    feat(packages): 添加template项目

---
+++ packages/template/src/main.ts
@@ -0,0 +1,11 @@
+import "amfe-flexible/index.js";
+import { createApp } from "vue";
+import router from "./router";
+import App from "./App.vue";
+
+import { sum } from "@hayeslc/shared";
+console.log(sum(1, 2));
+
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
